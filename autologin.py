d30842fb79d4b863cf011682d45b9ae2f806c9a8
EricTang <jeepxiaozi66@gmail.com>
2015-03-23 01:34:19 +0800

    诶，修改了几处错误。。
    脑袋真是秀逗掉了。。。犯这么萨比的错误

--- autologin.py
+++ autologin.py
@@ -44,29 +44,34 @@ def autoLogin(username, password):
 	login_response = s.post(login_url, data = form_data, headers = header)
 	login_res_content = login_response.content
 	# print(login_res_content)
-	if login_response.find("用户名或密码错误"):
-		print(u"登录错误，请检查核对一下用户名或密码")
-		return
-	elif login_response.find("docheckin"):
-		print(u"可以签到，即将进行签到操作...")
+	if login_res_content.find("用户名或密码错误") != -1:
+		writeResult("登录【" + username + "】时，用户名或者密码错误，请手动检查一下")
+	elif login_res_content.find("docheckin") != -1:
 		check_response = s.post(check_url, data = None, headers = header)
 		check_res_content = check_response.content
 		# print check_res_content
 		# 签到成功的话，获取一下该账号今日获得的流量
 		match_content = re.search("(签到成功.*流量).*", check_res_content)
-		print(match_content.group(1))
-		# 想了想还是保存到txt文本里，方便我自己查看的比较好
-		today_date = datetime.date.today()
-		result = open(today_date + "_shadowsocks.txt", "a")
-		result.write("账户【"+ username + "】"+ match_content.group(1) + "\n\n")
-		result.close()
-	elif login_response.find("不能签到"):
-		print(u"已经签到过了，次日方可以继续签到")
+		# 保存结果
+		checkin_result_str = "账户【" + username + "】" + match_content.group(1)
+		writeResult(checkin_result_str)
+	elif login_res_content.find("不能签到") != -1:
+		writeResult("账户【" + username + "】已经签过到了")
 #end def
 
+def writeResult(content):
+	"""
+	将签到结果保存到txt文档中
+	"""
+	# 想了想还是保存到txt文本里，方便我自己查看的比较好
+	today_date = datetime.date.today()
+	result = open(str(today_date) + "_shadowsocks.txt", "a")
+	result.write(content + "\n")
+	result.close()
+#end def
 if __name__ == "__main__":
 	"""
 	主方法
 	"""
 	for i in xrange(1,len(EricTang_Accounts)+1):
-		autoLogin(EricTang_Accounts[i]["username"], EricTang_Accounts[(i)]["password"])
+		autoLogin(EricTang_Accounts[i]["username"], EricTang_Accounts[i]["password"])
